e67e49a729742abe38e8d0e2d3b14cf480a6a45a
Sebastián Galiano <sgaliano@gmail.com>
2019-07-12 10:42:25 -0300

    Initializing registry with an event from a contract that was deployed very early instead of use a block handler

--- src/mappings/registry.ts
+++ src/mappings/registry.ts
@@ -1,17 +1,15 @@
-import { Address, EthereumBlock, JSONValue, Value, log, ipfs } from '@graphprotocol/graph-ts'
+import { Address, JSONValue, Value, log, ipfs } from '@graphprotocol/graph-ts'
 
 import { Token } from '../../generated/schema'
-import { ERC20Token } from '../../generated/TokenRegistry/templates'
+import { Unknown } from '../../generated/TokenRegistry/TokenRegistry'
+import { StandardToken } from '../../generated/TokenRegistry/templates'
 
 let REGISTRY_HASH = 'QmXuhRkxh7y6Gi1ZR8rEUWthcdKNWNbiEMpwzWpnMCRX6E'
 
-// TODO: Improve this approach when IPFS Bulk Import feature when it is fully implemented
-export function initRegistry(block: EthereumBlock): void {
-  let initialized = block.number.isI32() && block.number.toI32() > 1
+export function initRegistry(event: Unknown): void {
+  log.warning('Initializing token registry, block={}', [event.block.number.toString()])
 
-  if (!initialized) {
-    ipfs.mapJSON(REGISTRY_HASH, 'createToken', Value.fromString(''))
-  }
+  ipfs.mapJSON(REGISTRY_HASH, 'createToken', Value.fromString(''))
 }
 
 export function createToken(value: JSONValue, userData: Value): void {
@@ -22,18 +20,19 @@ export function createToken(value: JSONValue, userData: Value): void {
   let token = Token.load(address.toHex())
 
   if (!token) {
-    let flags = data.get('flags').toArray().map<string>(value => value.toString())
-
     let name = data.get('name').toString()
     let symbol = data.get('symbol').toString()
     let decimals = data.get('decimals').toBigInt().toI32()
+    let flags = data.get('flags').toArray().map<string>(value => value.toString())
+    // TODO: let description = data.get('description').toString()
+    // TODO: let imageUrl = data.get('image').toString()
 
     log.debug('Adding token to registry, name: {}, symbol: {}, address: {}, decimals: {}, flags: {}', [
       name,
       symbol,
       address.toHex(),
       decimals.toString(),
-      flags.length ? flags.join('|') : 'none',
+      flags.length ? flags.join('|') : 'none'
     ])
 
     token = new Token(address.toHex())
@@ -41,14 +40,13 @@ export function createToken(value: JSONValue, userData: Value): void {
     token.name = name
     token.symbol = symbol
     token.decimals = decimals
-
-    // TODO: token.description = data.get('description').toString()
-    // TODO: token.imageUrl = data.get('image').toString()
+    // TODO: token.description = description
+    // TODO: token.imageUrl = imageUrl
 
     token.save()
 
     // Start indexing token events
-    ERC20Token.create(address)
+    StandardToken.create(address)
   } else {
     log.warning('Token {} already in registry', [address.toHex()])
   }
