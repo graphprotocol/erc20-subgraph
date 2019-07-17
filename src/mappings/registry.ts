64241903d69ec99c3fd1218986c884c6d301946d
Sebastián Galiano <sgaliano@gmail.com>
2019-07-17 16:49:26 -0300

    Simplify token registry structure. Improve token flags handling.

--- src/mappings/registry.ts
+++ src/mappings/registry.ts
@@ -4,7 +4,8 @@ import { Token } from '../../generated/schema'
 import { Unknown } from '../../generated/TokenRegistry/TokenRegistry'
 import { BurnableToken, MintableToken, StandardToken } from '../../generated/TokenRegistry/templates'
 
-let REGISTRY_HASH = 'QmXuhRkxh7y6Gi1ZR8rEUWthcdKNWNbiEMpwzWpnMCRX6E'
+const DEFAULT_DECIMALS = 18
+const REGISTRY_HASH = 'QmUZ732EtRbQSXWGDtHwnQF68vbn1EGq1czapr4EYFpWDJ'
 
 export function initRegistry(event: Unknown): void {
   log.debug('Initializing token registry, block={}', [event.block.number.toString()])
@@ -13,43 +14,83 @@ export function initRegistry(event: Unknown): void {
 }
 
 export function createToken(value: JSONValue, userData: Value): void {
-  let data = value.toObject()
+  let rawData = value.toArray()
 
-  let address = Address.fromString(data.get('address').toString())
+  let address: string | null = rawData[0].isNull() ? null : rawData[0].toString()
+  let name: string | null = rawData[1].isNull() ? null : rawData[1].toString()
+  let symbol: string | null = rawData[2].isNull() ? null : rawData[2].toString()
+  let decimals: u32 = rawData[3].isNull() ? DEFAULT_DECIMALS : rawData[3].toBigInt().toI32()
+  let description: string | null = rawData[4].isNull() ? null : rawData[4].toString()
+  let imageUrl: string | null = rawData[5].isNull() ? null : rawData[5].toString()
+  let flags: u64 = rawData[6].isNull() ? 0 : rawData[6].toU64()
 
-  let token = Token.load(address.toHex())
+  if (address) {
+    let contractAddress = Address.fromString(address)
 
-  if (!token) {
-    let name = data.get('name').toString()
-    let symbol = data.get('symbol').toString()
-    let decimals = data.get('decimals').toBigInt().toI32()
-    let flags = data.get('flags').toArray().map<string>(value => value.toString())
-    // TODO: let description = data.get('description').toString()
-    // TODO: let imageUrl = data.get('image').toString()
+    let token = Token.load(contractAddress.toHex())
 
-    log.debug('Adding token to registry, name: {}, symbol: {}, address: {}, decimals: {}, flags: {}', [
-      name,
-      symbol,
-      address.toHex(),
-      decimals.toString(),
-      flags.length ? flags.join('|') : 'none'
-    ])
+    if (!token) {
+      token = new Token(contractAddress.toHex())
+      token.address = contractAddress
+      token.name = name
+      token.symbol = symbol
+      token.decimals = decimals
+      token.description = description
+      token.imageUrl = imageUrl
+      token.flags = decodeFlags(flags)
 
-    token = new Token(address.toHex())
-    token.address = address
-    token.name = name
-    token.symbol = symbol
-    token.decimals = decimals
-    // TODO: token.description = description
-    // TODO: token.imageUrl = imageUrl
+      log.debug('Adding token to registry, name: {}, symbol: {}, address: {}, decimals: {}, flags: {}', [
+        token.name,
+        token.symbol,
+        token.id,
+        decimals.toString(), // TODO: use token.decimals.toString() when type 'i32' implements toString()
+        token.flags.length ? token.flags.join('|') : 'none'
+      ])
 
-    token.save()
+      token.save()
 
-    // Start indexing token events
-    StandardToken.create(address)
-    BurnableToken.create(address)
-    MintableToken.create(address)
-  } else {
-    log.warning('Token {} already in registry', [address.toHex()])
+      // Start indexing token events
+      StandardToken.create(contractAddress)
+
+      if (isBurnable(flags)) {
+        BurnableToken.create(contractAddress)
+      }
+
+      if (isMintable(flags)) {
+        MintableToken.create(contractAddress)
+      }
+    } else {
+      log.warning('Token {} already in registry', [contractAddress.toHex()])
+    }
   }
+}
+
+function decodeFlags(value: u64): string[] {
+  let flags: string[] = []
+
+  if (isDetailed(value)) {
+    flags.push('detailed')
+  }
+
+  if (isBurnable(value)) {
+    flags.push('burnable')
+  }
+
+  if (isMintable(value)) {
+    flags.push('mintable')
+  }
+
+  return flags
+}
+
+function isDetailed(flags: u64): boolean {
+  return (flags & (2 << 0)) != 0
+}
+
+function isBurnable(flags: u64): boolean {
+  return (flags & (2 << 1)) != 0
+}
+
+function isMintable(flags: u64): boolean {
+  return (flags & (2 << 3)) != 0
 }
