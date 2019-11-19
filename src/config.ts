6580365a47977807b121279749b7127b475bf73f
Sebastián Galiano <sgaliano@gmail.com>
2019-11-19 12:57:49 -0300

    Add source JSON file for the token list

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const REGISTRY_HASH = 'QmS91QaQozMhQzT68yRxXz7SL2a3snixX9WXiyVNPaVDt8'
+export const REGISTRY_HASH = 'QmS91QaQozMhQzT68yRxXz7SL2a3snixX9WXiyVNPaVDt8' // data/tokens-finance.json
